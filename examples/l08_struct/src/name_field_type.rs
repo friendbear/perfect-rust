29da6d5d0b9c97bcd16328b251314791d04f5d4a
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-08-18 10:12:18 +0900

    名前付きフィールド型

---
+++ examples/l08_struct/src/name_field_type.rs
@@ -0,0 +1,45 @@
+pub struct Customer {
+    id: i32,
+    name: String,
+    address: String,
+    email: String,
+}
+pub struct Member<'a> {
+    id: i32,
+    name: &'a str,
+    address: &'a str,
+    email: &'a str,
+}
+
+#[cfg(test)]
+mod test_name_field_type {
+    use super::*;
+    #[test]
+    fn generate_instance() {
+        let customer = Customer {
+            id: 100,
+            name: String::from("山田太郎"),
+            address: String::from("東京都新宿区"),
+            email: String::from("yamada@sample.com"),
+        };
+        assert!(
+            customer.id == 100
+                && customer.name.eq("山田太郎")
+                && customer.address.eq("東京都新宿区")
+                && customer.email.eq("yamada@sample.com")
+        );
+
+        let member = Member {
+            id: 200,
+            name: "山田花子",
+            address: "東京都港区",
+            email: "yamada@sample.com",
+        };
+        assert!(
+            member.id == 200
+                && member.name.eq("山田花子")
+                && member.address.eq("東京都港区")
+                && member.email.eq("yamada@sample.com")
+        );
+    }
+}
