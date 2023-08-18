9a3bf7f53eb19bfc7a6a0cc5fd2a05b9264832d6
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-08-18 10:29:56 +0900

    タプル型とユニット型

--- examples/l08_struct/src/lib.rs
+++ examples/l08_struct/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod name_field_type;
+pub mod tuple_unit_type;
---
+++ examples/l08_struct/src/tuple_unit_type.rs
@@ -0,0 +1,28 @@
+
+/// 座標を表すタプル型構造体
+struct Coordinate(usize, usize);
+
+/// ユニット型構造体の生成
+struct OneState;
+
+#[cfg(test)]
+mod test_tuple_unit_type {
+    use super::{Coordinate, OneState};
+    use std::borrow::Borrow;
+    #[test]
+    fn generate_tuple() {
+        let coordinate = Coordinate(100, 200);
+        assert!(coordinate.0 == 100 && coordinate.1 == 200);
+    }
+
+    #[test]
+    fn generate_unit() {
+        let one_state = OneState;
+        let two_state = OneState;
+
+        let one = one_state.borrow();
+        let two = two_state.borrow();
+        eprintln!("unit one addr = {:p}", one);
+        eprintln!("unit two addr = {:p}", two);
+    }
+}
