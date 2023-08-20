78115a089d6a5b3c7229c87a022f7d86cb9acb41
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-08-20 14:45:58 +0900

    commit leak.

---
+++ examples/l10_trait/src/generics/service.rs
@@ -0,0 +1,31 @@
+use super::traits::{CsvReader, JsonReader};
+use super::traits_impl::{CsvReaderImpl, JsonReaderImpl};
+use anyhow::Result;
+use serde::de::DeserializeOwned;
+
+
+pub struct ReadService<T> {
+    // CsvReaderトレイトの実装型フィールド
+    csv_reader: Box<dyn CsvReader<T>>,
+    json_reader: Box<dyn JsonReader<T>>,
+}
+
+impl<T> ReadService<T>
+where
+    T: DeserializeOwned + 'static,
+{
+    pub fn new() -> Self {
+        Self {
+            csv_reader: Box::new(CsvReaderImpl::<T>::new()) as Box<dyn CsvReader<T>>,
+            json_reader: Box::new(JsonReaderImpl::<T>::new()) as Box<dyn JsonReader<T>>,
+        }
+    }
+
+    pub fn csv_reader(&self, file_path: &str) -> Result<Vec<T>> {
+        self.csv_reader.read(file_path)
+    }
+
+    pub fn json_reader(&self, file_path: &str) -> Result<Vec<T>> {
+        self.json_reader.read(file_path)
+    }
+}
