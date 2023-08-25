afeac34dfbf6f8a5e5526f5408edba1d874bf2ec
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-08-25 14:33:03 +0900

    should_panic, ignore, and more.

--- practice-collection/testing/src/target.rs
+++ practice-collection/testing/src/target.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Formatter, Debug};
-use std::error::Error;
 
 /// サンプルコードで利用するエラー列挙型
+#[allow(dead_code)]
 #[derive(Debug, Eq, PartialEq)]
 enum SampleError {
     Msg(String)
@@ -26,12 +26,14 @@ impl Display for Guest {
     }
 }
 impl Guest {
+    #[allow(dead_code)]
     pub fn new(age: u32, campaign: bool) -> Self {
         Self {age, campaign}
     }
 
     /// 鑑賞金額を計算する
-    pub fn calc_fee(self) -> Result<u32, SampleError> {
+    #[allow(dead_code)]
+    pub fn calc_fee(&self) -> Result<u32, SampleError> {
         let fee = match self.age {
             0..=4 => 0,
             5..=12 => 500,
@@ -43,6 +45,7 @@ impl Guest {
         Ok(self.calc_campaign(fee))
     }
     /// キャンペーン中は10%引きにする
+    #[allow(dead_code)]
     fn calc_campaign(&self, mut fee: u32) -> u32 {
         if self.campaign && fee != 0 {
             fee = fee * 90 / 100;
@@ -53,9 +56,77 @@ impl Guest {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[ignore = "テスト実装前"]
     #[test]
-    fn calc_fee_case01() {
+    fn calc_fee_case00() {
 
+    }
+    #[test]
+    fn calc_fee_case01() {
+        let guest = Guest::new(10, false);
+        let result = guest.calc_fee().unwrap();
+        assert!(result == 500, "{}", &guest);
+    }
+    #[test]
+    fn calc_fee_case_campaign_01() {
+        let guest = Guest::new(10, true);
+        let result = guest.calc_fee().unwrap();
+        assert!(result == 450, "{}", &guest);
+    }
+    #[test]
+    fn calc_fee_case_02() {
+        let guest = Guest::new(15, false);
+        let result = guest.calc_fee().unwrap();
+        assert_eq!(700, result, "{}", &guest);
+    }
+    #[test]
+    fn calc_fee_case_campaign_02() {
+        let guest = Guest::new(15, true);
+        let result = guest.calc_fee().unwrap();
+        assert_eq!(630, result, "{}", &guest);
+    }
+    #[test]
+    fn calc_fee_case_03() {
+        let guest = Guest::new(18, false);
+        let result = guest.calc_fee().unwrap();
+        assert_ne!(700, result, "{}", &guest);
+    }
+    #[test]
+    fn calc_fee_case_campaign_03() {
+        let guest = Guest::new(15, true);
+        let result = guest.calc_fee().unwrap();
+        assert_ne!(700, result, "{}", &guest);
+    }
+    #[test]
+    fn calc_fee_wrong_agge() {
+        let guest = Guest::new(125, false);
+        let actual = guest.calc_fee().unwrap_err();
+        let expected = SampleError::Msg(String::from("Age is incorrect."));
+        assert_eq!(expected, actual, "{}", &actual);
+    }
+    #[test]
+    fn calc_campagin_fee_case_01(){
+        let guest = Guest::new(1000, true);
+        let actual = guest.calc_campaign(1000);
+        assert_eq!(900, actual);
+    }
+    #[test]
+    #[should_panic(expected="Age is incorrect.")]
+    fn calc_fee_should_panic() {
+        let guest = Guest::new(130, true);
+        match guest.calc_fee() {
+            Ok(_) => {},
+            Err(e) => panic!("{}", e),
+        }
+    }
+    /// Debug dbg!() 
+    #[test]
+    fn use_debug() {
+        let guest = Guest::new(0, false);
+        dbg!(&guest);
+        let actual = guest.calc_fee().unwrap();
+        assert_eq!(0, actual);
     }
 }
