ce2d8c35bf1179b11272657465bb4765d1f45996
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-08-31 16:16:42 +0900

    コネクションプール

--- practice_collection/postgres/src/lib.rs
+++ practice_collection/postgres/src/lib.rs
@@ -5,3 +5,6 @@ mod entities;
 mod repository;
 mod product_repository;
 mod product_category_repository;
+mod pool_1;
+mod pool_2;
+mod manager;
---
+++ practice_collection/postgres/src/pool_1.rs
@@ -0,0 +1,19 @@
+use std::sync::Mutex;
+use r2d2_postgres::{postgres::NoTls, PostgresConnectionManager};
+use r2d2_postgres::r2d2::Pool;
+use once_cell::sync::Lazy;
+
+#[allow(dead_code)]
+// Connecttion String
+static CONNECTION_STR: &str = "host=localhost port=5432 dbname=rust_sample user=postgres password=admin";
+#[allow(dead_code)]
+// Create Connection Pool
+pub static SAMPLE_POOL_1: Lazy<Mutex<Pool<PostgresConnectionManager<NoTls>>>> = 
+Lazy::new(|| {
+    let config = CONNECTION_STR.parse().unwrap();
+    // create connection manager
+    let manager = PostgresConnectionManager::new(config, NoTls);
+    // create pool
+    let pool = r2d2::Pool::new(manager).unwrap();
+    Mutex::new(pool)
+});
