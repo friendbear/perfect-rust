96e104a5b86a2ef83ee2b336bb25efbb267565b4
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-09-01 07:28:06 +0900

    [cargo clippy warning]arning: this `MutexGuard` is held across an `await` point

--- practice_collection/postgres/src/tokio_connect.rs
+++ practice_collection/postgres/src/tokio_connect.rs
@@ -1,20 +1,18 @@
-use crate::params::ConnectParams;
 use anyhow::Result;
+use tokio_postgres::{Client, NoTls};
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
-use tokio_postgres::{Client, NoTls};
+use crate::params::ConnectParams;
 
 #[allow(dead_code)]
-pub static CONNECT_PARAMS: Lazy<Mutex<ConnectParams>> = Lazy::new(|| {
-    let params = ConnectParams::new(
-        "localhost".to_owned(),
-        5432,
-        "rust_sample".to_owned(),
-        "postgres".to_owned(),
-        "admin".to_owned(),
-    );
-    Mutex::new(params)
-});
+pub static CONNECT_PARAMS: Lazy<Mutex<ConnectParams>> =
+    Lazy::new(||{
+        let params = ConnectParams::new(
+            "localhost".to_owned(), 5432, "rust_sample".to_owned(),
+            "postgres".to_owned(), "admin".to_owned()
+        );
+        Mutex::new(params)
+    });
 
 /// 非同期実行データベース接続機能
 #[allow(dead_code)]
@@ -22,15 +20,19 @@ pub struct AsyncSimpleClient;
 impl AsyncSimpleClient {
     #[allow(dead_code)]
     pub async fn connect() -> Result<Client> {
-        let params = CONNECT_PARAMS.lock().unwrap();
-        let (client, connection) =
-            tokio_postgres::connect(params.connect_string().as_str(), NoTls).await?;
-
-        tokio::spawn(async move {
+        let config;
+        {
+            let params = CONNECT_PARAMS.lock().unwrap();  // params lock here.
+            config = params.connect_string().clone();
+        } // params drop here.
+        //let config = CONNECT_PARAMS.lock().unwrap().connect_string().clone();
+        let (client, connection) = tokio_postgres::connect(config.as_str(), NoTls).await?;
+        let handle = tokio::spawn(async move {
             if let Err(e) = connection.await {
-                panic!("{}", e);
+                eprintln!("Connection error: {}", e);
             }
         });
+        handle.await.expect("Async task panicked");
         Ok(client)
     }
 }
