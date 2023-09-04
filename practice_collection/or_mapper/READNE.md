8e3efeaaa895419aa3258815f5163899c6139129
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-09-04 20:01:11 +0900

    マイグレーション機能の利用

--- practice_collection/or_mapper/READNE.md
+++ practice_collection/or_mapper/READNE.md
@@ -1,1 +1,24 @@
 # O/R Mapper
+
+## SeaORM
+
+```sh
+cargo install sea-orm-cli
+```
+
+### How to use.
+
+default database url .env
+
+sea-orm-cli [-h|-v|-V]
+
+sub commands
+
+* generate
+* help
+* migrate
+
+```sh
+sea-orm-cli generate entity -u postgres://usr:pass@host:5432/dbname -o src/modules \
+    --with-serde (none, serialize, deserialize, both) [default: none]
+```
---
+++ practice_collection/or_mapper/src/pool.rs
@@ -0,0 +1,24 @@
+use dotenv::dotenv;
+use std::env;
+use std::time::Duration;
+use anyhow::Result;
+use sea_orm::{DatabaseConnection, Database, ConnectOptions};
+
+pub struct SamplePool;
+impl SamplePool {
+
+    pub async fn get() -> Result<DatabaseConnection> {
+        dotenv().ok(); // convert Result to Option
+        let database_url = env::var("DATABASE_URL")?;
+        let mut options = ConnectOptions::new(database_url);
+        options.max_connections(10)
+            .min_connections(5)
+            .connect_timeout(Duration::from_secs(10))
+            .idle_timeout(Duration::from_secs(10))
+            .max_lifetime(Duration::from_secs(10))
+            .sqlx_logging(true);
+
+        let conn = Database::connect(options).await?;
+        Ok(conn)
+    }
+}
