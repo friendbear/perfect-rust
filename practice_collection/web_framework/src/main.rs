5689d0143baec0d38d18e45b29986e98b04a819a
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-09-09 12:22:02 +0900

    Http Serverの起動

--- practice_collection/web_framework/src/main.rs
+++ practice_collection/web_framework/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, middleware, web};
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+    }).bind("127.0.0.1:8081")?.workers(2).run().await
 }
