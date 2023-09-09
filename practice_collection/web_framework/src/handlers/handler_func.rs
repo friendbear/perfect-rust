675b5133828d20bab8c944715aeffaa5cc9470dc
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-09-09 13:47:38 +0900

    resource() method

--- practice_collection/web_framework/src/handlers/handler_func.rs
+++ practice_collection/web_framework/src/handlers/handler_func.rs
@@ -1,12 +1,23 @@
-
-use actix_web::{get, Responder, web, HttpResponse};
+use actix_web::{get, web, HttpResponse, Responder};
 use mime;
-#[get("/calc/sum/{value1}/{value2}")]
-pub async fn calc_sum(value: web::Path<(String, String)>) -> impl Responder {
+#[get("/calc/add/{value1}/{value2}")]
+pub async fn calc_add(value: web::Path<(String, String)>) -> impl Responder {
     let v1 = value.0.parse::<i32>().unwrap();
     let v2 = value.1.parse::<i32>().unwrap();
 
     let response = format!("{v1} + {v2} = {}", v1 + v2);
 
-    HttpResponse::Ok().content_type(mime::TEXT_PLAIN).body(response)
+    HttpResponse::Ok()
+        .content_type(mime::TEXT_PLAIN)
+        .body(response)
+}
+pub async fn calc_sub(value: web::Path<(String, String)>) -> impl Responder {
+    let v1 = value.0.parse::<i32>().unwrap();
+    let v2 = value.1.parse::<i32>().unwrap();
+
+    let response = format!("{v1} - {v2} = {}", v1.wrapping_sub(v2));
+
+    HttpResponse::Ok()
+        .content_type(mime::TEXT_PLAIN)
+        .body(response)
 }
--- practice_collection/web_framework/src/main.rs
+++ practice_collection/web_framework/src/main.rs
@@ -1,26 +1,34 @@
 pub mod handlers;
 
-use actix_web::{App, HttpServer, middleware, web};
+use actix_web::{
+    middleware,
+    web::{self, resource},
+    App, HttpServer,
+};
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .service(
-                web::scope("/app")
-                    .service(
-                        web::scope("/v1")
-                        .service(web::resource("/ping").to(ping))
-                    )
-                    .service(
-                        web::scope("/example")
-                        .service(handlers::handler_func::calc_sum)
-                    )
-            )
-    }).bind("127.0.0.1:8081")?.workers(2).run().await
+        App::new().wrap(middleware::Logger::default()).service(
+            web::scope("/app")
+                .service(web::scope("/v1").service(web::resource("/ping").to(ping)))
+                .service(
+                    web::scope("/example")
+                        .service(handlers::handler_func::calc_add)
+                        .service(
+                            resource("/calc/sub/{value1}/{value2}")
+                                .route(web::get().to(handlers::handler_func::calc_sub))
+                                .route(web::post().to(handlers::handler_func::calc_sub)),
+                        ),
+                ),
+        )
+    })
+    .bind("127.0.0.1:8081")?
+    .workers(2)
+    .run()
+    .await
 }
 
 async fn ping() -> &'static str {
