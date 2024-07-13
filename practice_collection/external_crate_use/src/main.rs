c7713eee24120cc42a33912bcd7f2ac8b0afd2f7
T Kumagai <4956799+friendbear@users.noreply.github.com>
2024-07-13 12:46:43 +0900

    YouTube to mp3 v3

--- practice_collection/external_crate_use/src/main.rs
+++ practice_collection/external_crate_use/src/main.rs
@@ -1,13 +1,13 @@
+use actix_session::config::BrowserSession;
+use actix_session::storage::CookieSessionStore;
+use actix_session::Session;
+use actix_session::SessionMiddleware;
 use actix_web::{
+    cookie::time::Duration,
     middleware,
     web::{self, resource, ServiceConfig},
-    cookie::time::Duration,
-    App, HttpServer, HttpResponse, Responder, Error
+    App, Error, HttpResponse, HttpServer, Responder,
 };
-use actix_session::config::BrowserSession;
-use actix_session::SessionMiddleware;
-use actix_session::Session;
-use actix_session::storage::CookieSessionStore;
 use openssl::ssl::{SslAcceptor, SslAcceptorBuilder, SslFiletype, SslMethod};
 
 #[actix_web::main]
@@ -17,36 +17,26 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-        .wrap(middleware::Logger::default())
-        .wrap(middleware::Logger::new("%a %{User-Agent}i"))
-        .wrap(middleware::DefaultHeaders::new().add(("X-Version", "0.2")))
-        .wrap(
-            SessionMiddleware::builder(
-                CookieSessionStore::default(), key.clone()
+            .wrap(middleware::Logger::default())
+            .wrap(middleware::Logger::new("%a %{User-Agent}i"))
+            .wrap(middleware::DefaultHeaders::new().add(("X-Version", "0.2")))
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
+                    .session_lifecycle(BrowserSession::default().state_ttl(Duration::minutes(5)))
+                    .cookie_name("rsessionid".to_string())
+                    .build(),
             )
-            .session_lifecycle(
-                BrowserSession::default().state_ttl(Duration::minutes(5))
-            )
-            .cookie_name("rsessionid".to_string())
-            .build()
-        )
-        .service(
-            web::scope("/v2")
-            .configure(set_configure)
-        )
-        .service(web::resource("/").to(index))
-//        .route("/", web::get().to(|| HttpResponse::Ok)
-
-    }).bind_openssl("127.0.0.1:8082", create_ssl_accepter_builder())?
+            .service(web::scope("/v2").configure(set_configure))
+            .service(web::resource("/").to(index))
+        //        .route("/", web::get().to(|| HttpResponse::Ok)
+    })
+    .bind_openssl("127.0.0.1:8082", create_ssl_accepter_builder())?
     .run()
     .await
 }
 
 fn set_configure(cfg: &mut ServiceConfig) {
-    cfg.service(
-        resource("/health")
-        .route(web::get().to(health))
-    );
+    cfg.service(resource("/health").route(web::get().to(health)));
 }
 async fn index(session: Session) -> Result<HttpResponse, Error> {
     // access session data
@@ -63,15 +53,18 @@ async fn index(session: Session) -> Result<HttpResponse, Error> {
 }
 
 async fn health() -> impl Responder {
-
-    HttpResponse::Ok().content_type(mime::APPLICATION_JSON).body(r#"{"health": "Ok"}"#)
+    HttpResponse::Ok()
+        .content_type(mime::APPLICATION_JSON)
+        .body(r#"{"health": "Ok"}"#)
 }
 // openssl crate
 fn create_ssl_accepter_builder() -> SslAcceptorBuilder {
     // OpenSSL構造を管理し、暗号スイート、セッションオプションなどを構成する
     let mut builder: SslAcceptorBuilder =
         SslAcceptor::mozilla_intermediate_v5(SslMethod::tls_server()).unwrap();
-    builder.set_private_key_file("local-key.pem", SslFiletype::PEM).unwrap();
+    builder
+        .set_private_key_file("local-key.pem", SslFiletype::PEM)
+        .unwrap();
     builder.set_certificate_chain_file("local.pem").unwrap();
     builder
 }
--- practice_collection/youtube_to_mp3/src/bin/youtube_to_mp3.rs
+++ practice_collection/youtube_to_mp3/src/bin/youtube_to_mp3.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 use std::env;
 use std::process::Command;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
-// Remove the unused import
+//use serde_json::*;
 
 // API Response のjson を構造体で定義
 #[derive(Serialize, Deserialize, Debug)]
@@ -28,18 +28,26 @@ struct Snippet {
     title: String,
 }
 
+const MAX_RESULTS: u32 = 50;
+
 fn search_videos(api_key: &str, query: &str) -> Result<Vec<SearchResult>, Box<dyn Error>> {
-    let url = format!("https://www.googleapis.com/youtube/v3/search?key={}&part=snippet&q={}&maxResults=1&type=video", api_key, query);
+    let url = format!("https://www.googleapis.com/youtube/v3/search?key={}&part=snippet&q={}&maxResults={}&type=video", api_key, query, MAX_RESULTS);
     let client = Client::new();
     let response = client.get(&url).send()?.json::<SearchResponse>()?;
+    //println!("{:?}", seade_json::to_json::<SearchResponse>(response));
     Ok(response.items)
 }
+fn sanitize_filename(filename: &str) -> String {
+    filename.replace("/", "_")
+}
 
-fn download_video(video_id: &str, output_dir: &str) -> Result<String, Box<dyn Error>> {
+fn download_video(video_id: &str, output_dir: &str) -> Result<PathBuf, Box<dyn Error>> {
     let video_url = format!("https://www.youtube.com/watch?v={}", video_id);
-    let output_path = Path::new(output_dir).join(format!("{}.mp4", video_id));
+    let output_path = Path::new(output_dir).join(format!("{}.webm", video_id));
 
-    let status = Command::new("youtube-dl")
+    let status = Command::new("yt-dlp")
+        .arg("-f")
+        .arg("bestvideo[ext=webm]+bestaudio[ext=webm]/best[ext=webm]")
         .arg("-o")
         .arg(output_path.to_string_lossy().to_string())
         .arg(&video_url)
@@ -49,15 +57,21 @@ fn download_video(video_id: &str, output_dir: &str) -> Result<String, Box<dyn Er
         return Err("Failed to download video".into());
     }
 
-    Ok(output_path.to_string_lossy().to_string())
+    Ok(output_path)
 }
 
-fn convert_to_mp3(input_path: &str, output_dir: &str, title: &str) -> Result<(), Box<dyn Error>> {
-    let output_path = Path::new(output_dir).join(format!("{}.mp3", title));
+fn convert_to_mp3(input_path: &Path, output_dir: &str, title: &str) -> Result<(), Box<dyn Error>> {
+    let sanitized_title = sanitize_filename(title);
+    let output_path = Path::new(output_dir).join(format!("{}.mp3", sanitized_title));
+
+    // 出力ディレクトリが存在しない場合は作成
+    if let Some(parent) = output_path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
 
     let status = Command::new("ffmpeg")
         .arg("-i")
-        .arg(input_path)
+        .arg(input_path.to_string_lossy().to_string())
         .arg(output_path.to_string_lossy().to_string())
         .status()?;
 
@@ -75,30 +89,21 @@ fn main() {
     let query = env::var("YOUTUBE_QUERY").unwrap_or("Hatsune Miku".to_string());
     let download_dir = env::var("DOWNLOAD_DIR").expect("DOWNLOAD_DIR must be set");
 
-    print!("Searching for videos... {query}");
-    match search_videos(&api_key, &query) {
-        Ok(results) => {
-            if let Some(video) = results.first() {
-                let video_id = &video.id.videoId;
-                let title = &video.snippet.title;
+    for video in search_videos(&api_key, &query).unwrap() {
+        let video_id = &video.id.videoId;
+        let title = &video.snippet.title;
 
-                println!("Found video: {} ({})", title, video_id);
-                
-                match download_video(video_id, &download_dir) {
-
-                    Ok(video_path) => {
-                        if let Err(err) = convert_to_mp3(&video_path, &download_dir, title) {
-                            eprintln!("Failed to convert video to mp3: {}", err);
-                        } else {
-                            println!("Video converted to mp3 successfully!");
-                        }
-                    },
-                    Err(err) => eprintln!("Failed to download video: {}", err),
+        println!("Found video: {} ({})", title, video_id);
+        
+        match download_video(video_id, &download_dir) {
+            Ok(video_path) => {
+                if let Err(err) = convert_to_mp3(&video_path, &download_dir, title) {
+                    eprintln!("Failed to convert video to mp3: {}", err);
+                } else {
+                    println!("Video converted to mp3 successfully!");
                 }
-            } else {
-                eprintln!("No videos found for the query.");
-            }
-        },
-        Err(err) => eprintln!("Search failed: {}", err),
+            },
+            Err(err) => eprintln!("Failed to download video: {}", err),
+        }
     }
 }
